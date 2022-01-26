6c79da0690e58e05d1cfd4b8bded033a0f4eb393
Łukasz Czepielik <l.czepielik@gmail.com>
2022-01-26 09:18:44 +0100

    Added basic Entities structure.

---
+++ EquipmentInventoryAPI.DataAccess/Models/DeviceModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipmentInventoryAPI.DataAccess.Models
+{
+    public class DeviceModel
+    {
+        public int Id { get; set; }
+        public int ModelNumber { get; set; }
+
+        public Manufacturer Manufacturer { get; set; }
+
+        public DeviceModel()
+        {
+
+        }
+    }
+}
---
+++ EquipmentInventoryAPI.DataAccess/Models/IDevice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipmentInventoryAPI.DataAccess.Models
+{
+    public abstract class IDevice
+    {
+        public int Id { get; set; }
+        public string SerialNumber { get; set; }
+
+        public string Name { get; set; }
+        public string PurchasePrice { get; set; }
+        public string PresentPrice { get; set; }
+
+        public DateTime PurchaseDate { get; set; }
+
+        public DeviceModel Model { get; set; } 
+    }
+}
