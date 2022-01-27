6cd87ba6575686a54c90a83a0eabf2f677330c14
Łukasz Czepielik <koragrex@gmail.com>
2022-01-27 11:39:32 +0100

    Expanded Seed method inside InMemoryContext.

--- EquipmentInventoryAPI.DataAccess/DbContext/InMemoryContext.cs
+++ EquipmentInventoryAPI.DataAccess/DbContext/InMemoryContext.cs
@@ -22,22 +22,24 @@ namespace EquipmentInventoryAPI.DataAccess.DbContext
                 Id = Guid.Parse("c579968b-4e44-40ac-a948-2abe3aefc054"),
                 SerialNumber = "ZXCZVGASG218",
                 Name = "Test notebook",
+                Type = AssetType.Notebook,
                 PresentPrice = 3200,
                 PurchasePrice = 3500,
-                PurchaseDate = DateTimeOffset.UtcNow,
+                PurchaseDate = DateTimeOffset.UtcNow.Subtract(TimeSpan.FromDays(3000)),
                 Owners = new List<Guid>()
                 {
                    Guid.Parse("c579968b-4e44-40ac-a948-2abe3aefc224")
                 }
-            }); ;
+            });
             assetsSet.Add(new Asset()
             {
-                Id = Guid.NewGuid(),
+                Id = Guid.Parse("c379968b-4e44-40ac-a948-2abe3aefc054"),
                 SerialNumber = "ZXCZVGAZG218",
                 Name = "Test notebook 2",
+                Type = AssetType.PC,
                 PresentPrice = 4200,
                 PurchasePrice = 5500,
-                PurchaseDate = DateTimeOffset.UtcNow,
+                PurchaseDate = DateTimeOffset.UtcNow.Subtract(TimeSpan.FromDays(2000)),
                 Owners = new List<Guid>()
                 {
                  Guid.Parse("c979968b-4e44-40ac-a948-2abe3aefc224")
@@ -45,17 +47,86 @@ namespace EquipmentInventoryAPI.DataAccess.DbContext
             });
             assetsSet.Add(new Asset()
             {
-                Id = Guid.NewGuid(),
+                Id = Guid.Parse("c179968b-4e44-40ac-a948-2abe3aefc054"),
                 SerialNumber = "DGHASJGDJA",
                 Name = "Test notebook 3",
+                Type = AssetType.Printer,
                 PresentPrice = 800,
                 PurchasePrice = 2800,
-                PurchaseDate = DateTimeOffset.UtcNow,
+                PurchaseDate = DateTimeOffset.UtcNow.Subtract(TimeSpan.FromDays(2000)),
                 Owners = new List<Guid>()
                 {
                     Guid.Parse("c579968b-4e44-40ac-a948-2abe3aefc224"),
                     Guid.Parse("c979968b-4e44-40ac-a948-2abe3aefc224")
                 }
+            });
+
+            usersSet.Add(new User()
+            {
+                Id = Guid.Parse("c579968b-4e44-40ac-a948-2abe3aefc224"),
+                FirstName = "Test",
+                LastName = "User",
+                EmailAddress = "test.user@email.com",
+                ContactPhone = "123456789"
+            });
+            usersSet.Add(new User()
+            {
+                Id = Guid.Parse("c979968b-4e44-40ac-a948-2abe3aefc224"),
+                FirstName = "Super",
+                LastName = "User",
+                EmailAddress = "super.user@email.com",
+                ContactPhone = "987654321"
+            });
+
+            usersAssetsSet.Add(new UserAssetsOwnership()
+            {
+               UserId = Guid.Parse("c579968b-4e44-40ac-a948-2abe3aefc224"),
+               Assets = new List<UserAsset>()
+               {
+                   new UserAsset()
+                   {
+                       Id = Guid.NewGuid(),
+                       AssetId = Guid.Parse("c579968b-4e44-40ac-a948-2abe3aefc054"),
+                       AquireDate = DateTimeOffset.UtcNow.Subtract(TimeSpan.FromDays(2500)),
+                       DisposalDate = null
+                   },
+                   new UserAsset()
+                   {
+                       Id = Guid.NewGuid(),
+                       AssetId = Guid.Parse("c179968b-4e44-40ac-a948-2abe3aefc054"),
+                       AquireDate = DateTimeOffset.UtcNow.Subtract(TimeSpan.FromDays(2000)),
+                       DisposalDate = null
+                   },
+                    new UserAsset()
+                   {
+                       Id = Guid.NewGuid(),
+                       AssetId = Guid.Parse("c379968b-4e44-40ac-a948-2abe3aefc054"),
+                       AquireDate = DateTimeOffset.UtcNow.Subtract(TimeSpan.FromDays(2000)),
+                       DisposalDate =  DateTimeOffset.UtcNow.Subtract(TimeSpan.FromDays(1800)),
+                   }
+               }
+            });
+
+            usersAssetsSet.Add(new UserAssetsOwnership()
+            {
+                UserId = Guid.Parse("c979968b-4e44-40ac-a948-2abe3aefc224"),
+                Assets = new List<UserAsset>()
+               {
+                   new UserAsset()
+                   {
+                       Id = Guid.NewGuid(),
+                       AssetId = Guid.Parse("c179968b-4e44-40ac-a948-2abe3aefc054"),
+                       AquireDate = DateTimeOffset.UtcNow.Subtract(TimeSpan.FromDays(2000)),
+                       DisposalDate = null
+                   },
+                    new UserAsset()
+                   {
+                       Id = Guid.NewGuid(),
+                       AssetId = Guid.Parse("c379968b-4e44-40ac-a948-2abe3aefc054"),
+                       AquireDate = DateTimeOffset.UtcNow.Subtract(TimeSpan.FromDays(1000)),
+                       DisposalDate =  null,
+                   }
+               }
             });
         }
     }
